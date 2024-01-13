88ae14fca6352b9feea1867404beba5d86a141d7
rustywriter <dhakalgrishma5@gmail.com>
2024-01-13 22:41:01 +0545

    some changes

--- lua/rustywriter/lazy/snippets.lua
+++ lua/rustywriter/lazy/snippets.lua
@@ -10,8 +10,8 @@ return {
     ls.filetype_extend("javascript", { "jsdoc" })
 
     vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-    vim.keymap.set({"i", "s"}, "<leader>;", function() ls.jump( 1) end, {silent = true})
-    vim.keymap.set({"i", "s"}, "<leader>,", function() ls.jump(-1) end, {silent = true})
+    vim.keymap.set({"i", "s"}, "<C+;>", function() ls.jump( 1) end, {silent = true})
+    vim.keymap.set({"i", "s"}, "<C+,>", function() ls.jump(-1) end, {silent = true})
 
     vim.keymap.set({"i", "s"}, "<C-E>", function()
       if ls.choice_active() then
